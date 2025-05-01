92ee678a4fcac6b5c6075df17800314f0988bbb6
DylanVL05 <127649711+DylanVL05@users.noreply.github.com>
2025-05-01 15:08:04 -0600

    Base front PH_ServiceCenter

---
+++ PH_ServiceCenter/PH_ServiceCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PH_ServiceCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
