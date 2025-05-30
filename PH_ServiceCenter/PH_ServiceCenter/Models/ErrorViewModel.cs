e13fc244642a31e741b1033a8299a5c50d23cebd
Bryan Enríquez Jiménez <benriquez60426@ufide.ac.cr>
2025-05-29 20:45:46 -0600

    Delete PH_ServiceCenter directory

--- PH_ServiceCenter/PH_ServiceCenter/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PH_ServiceCenter.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
