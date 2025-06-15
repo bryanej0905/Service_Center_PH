4fadf9aefb6575b163e7fb2a1ed0037c99e499b8
RoyRoldan17 <rroldan00033@ufide.ac.cr>
2025-06-14 20:51:02 -0600

    HU 3 Biblioteca
    Descargar en pdf cada proceso del modulo de la Biblioteca Didactica

--- ServiceCenter/Controllers/BibliotecaController.cs
+++ ServiceCenter/Controllers/BibliotecaController.cs
@@ -1,15 +1,17 @@
-﻿using System.Data.Entity;          
+﻿using ServiceCenter.Models;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Mvc;            
-using ServiceCenter.Models;
+using System.Web.Mvc;
+using Rotativa;
+using Rotativa.Options;
 
 namespace ServiceCenter.Controllers
 {
     [Authorize]
     public class BibliotecaController : Controller
     {
-        
+
         private readonly ApplicationDbContext _context;
 
         public BibliotecaController()
@@ -23,7 +25,6 @@ namespace ServiceCenter.Controllers
             // 1) Cargamos todas las guías incluyendo su categoría
             var query = _context.Guias.Include(g => g.Categoria).AsQueryable();
 
-            
             if (!string.IsNullOrWhiteSpace(searchString))
             {
                 query = query.Where(g =>
@@ -33,39 +34,35 @@ namespace ServiceCenter.Controllers
                 );
             }
 
-            
             var lista = await query.ToListAsync();
 
-           
             ViewBag.CurrentFilter = searchString;
 
             return View(lista);
         }
 
-      
         [HttpGet]
-        public async Task<ActionResult> Details(int id)
+        public async Task<ActionResult> Details(int? id)
         {
-            
+            if (id == null)
+                return HttpNotFound();
+
             var guia = await _context.Guias
                                      .Include(g => g.Categoria)
-                                     .FirstOrDefaultAsync(g => g.Id == id);
+                                     .FirstOrDefaultAsync(g => g.Id == id.Value);
 
             if (guia == null)
             {
-                
                 return HttpNotFound();
             }
 
             return View(guia);
         }
 
-      
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Rate(int id, bool isUseful)
         {
-            
             var guia = await _context.Guias.FindAsync(id);
             if (guia == null)
             {
@@ -77,17 +74,35 @@ namespace ServiceCenter.Controllers
             else
                 guia.NotUsefulCount++;
 
-            
             _context.Entry(guia).State = EntityState.Modified;
 
-            
             await _context.SaveChangesAsync();
 
-            
             return RedirectToAction("Details", new { id = id });
         }
 
-        
+        // ---------- NUEVO: Acción para descargar PDF ----------
+        [HttpGet]
+        public async Task<ActionResult> DescargarPDF(int id)
+        {
+            var guia = await _context.Guias
+                                     .Include(g => g.Categoria)
+                                     .FirstOrDefaultAsync(g => g.Id == id);
+
+            if (guia == null)
+            {
+                return HttpNotFound();
+            }
+
+            return new ViewAsPdf("GuiaPDF", guia)
+            {
+                FileName = $"{guia.Titulo.Replace(' ', '_')}.pdf",
+                PageSize = Size.A4,
+                PageOrientation = Orientation.Portrait,
+            };
+        }
+        // ------------------------------------------------------
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
